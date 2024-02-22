e2fcf2d80f69f920f3ecd4b07e275e330b626836
khawaja meer Afridi <khawajameerafridi683@gmail.com>
2024-02-22 08:18:34 +0500

    this is about the task

---
+++ task01.js
@@ -0,0 +1,35 @@
+// WELCOME MESSAGE
+// function displayWelcomeMessage() {
+//     var name = prompt("Please enter your ")
+//     alert("welcome,  " + name + "!") 
+// }
+// displayWelcomeMessage();
+
+
+
+// generateRandomNumber();
+// function generateQuote() {
+//     var quotes = [
+//         "The only way to do great work is to love what you do. – Steve Jobs",
+//         "Innovation distinguishes between a leader and a follower. – Steve Jobs",
+//         "Your time is limited, don't waste it living someone else's life. – Steve Jobs",
+//         "Stay hungry, stay foolish. – Steve Jobs",
+//         "The way to get started is to quit talking and begin doing. – Walt Disney",
+//         "All our dreams can come true if we have the courage to pursue them. – Walt Disney",
+//         "The more you like yourself, the less you are like anyone else, which makes you unique. – Walt Disney",
+//         "If you can dream it, you can do it. – Walt Disney"
+//     ];
+//     var randomIndex = Math.floor(Math.random() * quotes.length)
+//     return quotes[randomIndex];
+// }
+// var randomQuote = generateQuote();
+// alert(randomQuote);
+
+
+// RANDOM NUMBER GENERATOR
+// function generateRandomNumber() {
+//     var randomNumber = Math.floor(Math.random() * 100) + 1;
+//     return randomNumber;
+// }
+// var randomNum = generateRandomNumber();
+// console.log("Generated Random Number: " + randomNum);
